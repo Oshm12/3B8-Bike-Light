f8c0bd8a3713b60c9d992a4e3a9110cdc68d43b9
Oshm12 <44381890+Oshm12@users.noreply.github.com>
2021-02-09 11:04:38 +0000

    Rename Bike_light.ino to AccelerometerTest.ino

---
+++ AccelerometerTest.ino
@@ -0,0 +1,57 @@
+const int xInput = A0;
+const int yInput = A1;
+const int zInput = A2;
+const int buttonPin = 2;
+
+const int led = 13;
+
+int last_xRaw = 0;
+int last_time = 0;
+int timeInterval = 0;
+
+
+void setup() {
+  
+  Serial.begin(9600);
+  pinMode(led, OUTPUT);
+}
+
+void loop() {
+
+
+  int xRaw = analogRead(xInput);
+  int yRaw = analogRead(yInput);
+  int zRaw = analogRead(zInput);
+
+
+  Serial.print("\nX Axis: ");
+  Serial.print(xRaw);
+ /* Serial.print("\nY Axis: ");
+  Serial.print(yRaw);
+  Serial.print("\nZ Axis: ");
+  Serial.print(zRaw);
+*/
+
+  timeInterval = millis() - last_time;
+
+  if(abs(xRaw - last_xRaw > 200)){
+    if(timeInterval > 250){
+      
+      digitalWrite(led, HIGH);
+
+      last_time = millis();
+      delay(400);
+    }
+    
+  }
+
+  if(abs(xRaw - last_xRaw < 200)){
+
+    digitalWrite(led, LOW);
+  
+  }
+  last_xRaw = xRaw;
+
+  
+
+}
