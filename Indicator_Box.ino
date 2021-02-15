3dd6df89ce978a62e32a295ddbe9c97ec74ea988
Oshm12 <44381890+Oshm12@users.noreply.github.com>
2021-02-15 16:23:42 +0000

    Complete without Bluetooth

--- Indicator_Box.ino
+++ Indicator_Box.ino
@@ -1,6 +1,4 @@
-//Button inputs
-int left_button_pin = 9;
-int right_button_pin = 11;
+int right_button_pin = 9;
 int hazard_button_pin = 10;
 int button_interupt_pin = 2;
 
@@ -9,9 +7,15 @@ int left_LED = 4;
 int right_LED = 2; 
 int hazard_LED = 3;
 
+//Time set ups
 double left_start_t = 0;
 double right_start_t = 0;
 double hazard_start_t = 0;
+
+//Booleans for Tracking on/off
+bool left_indicator = false;
+bool right_indicator = false;
+bool hazards = false;
 
 void setup() {
   // put your setup code here, to run once:
@@ -50,34 +54,79 @@ void loop() {
   //Start
   if(digitalRead(left_button_pin)){
       //LEFT BUTTON pressed
+      if(!left_indicator){
+        //Turning on the light
+        Serial.println('l');
+        digitalWrite(left_LED, HIGH);
+        left_start_t = millis();
+        left_indicator = true;
+        
+        return;
+      }
 
-      //Serial.write(*left commmand here*);
-      Serial.println("LEFT on");
-      digitalWrite(left_LED, HIGH);
-      left_start_t = millis();
-      
-      return;
+      if(left_indicator && (millis() - left_start_t) > 750 ){
+        //turning off the light
+        Serial.println('L');
+        digitalWrite(left_LED, LOW);
+        left_indicator = false;
+        //delay to stop rapid flicking between on off
+        delay(400);
+
+        return;
+        
+       }
   }
+  
   if(digitalRead(right_button_pin)){
       //RIGHT BUTTON pressed
+      if(!right_indicator){
+        //Turning on the light
+        Serial.println('r');
+        digitalWrite(right_LED, HIGH);
+        right_start_t = millis();
+        right_indicator = true;
+  
+        return;
+      }
 
-      //Serial.write(*right commmand here*);
-      Serial.println("RIGHT on");
-      digitalWrite(right_LED, HIGH);
-      right_start_t = millis();
+      if(right_indicator && (millis() - right_start_t) > 750 ){
+         //Turning off the light
+         Serial.println('R');
+         digitalWrite(right_LED, LOW);
+         right_indicator = false;
+         //delay to stop rapid flicking between on off
+         delay(400);
 
-      return;
-    
+         return;
+        
+      }
   }
+  
   if(digitalRead(hazard_button_pin)){
       //HAZARD BUTTON pressed
 
-      //Serial.write(*hazard commmand here*);
-      Serial.println("HAZARD on");
-      digitalWrite(hazard_LED, HIGH);
-      hazard_start_t = millis();
+      if(!hazards){
+        //turning Hazards on
+        Serial.println('h');
+        digitalWrite(hazard_LED, HIGH);
+        hazard_start_t = millis();
+        hazards = true;
+  
+        return;
+      }
+      
+      if(hazards && (millis() - hazard_start_t) > 750 ){
+         //Turning off the light
+         Serial.println('H');
+         digitalWrite(hazard_LED, LOW);
+         hazards = false;
+         //delay to stop rapid flicking between on off
+         delay(400);
 
-      return;
+         return;
+        
+      }
+      
   }
 
   //end
@@ -98,10 +147,11 @@ void loop() {
     
   }
   
-  if(millis() - (hazard_start_t + 10000) > 0){
+  if(millis() - (hazard_start_t + 60000) > 0){
 
     digitalWrite(hazard_LED, LOW);
    // Serial.println("HAZARD LED OFF");
     
   }
 }
+
